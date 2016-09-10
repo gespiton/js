05d5b1ae02c7dfeff8c1ef4b1d8fc8a1552e1bcb
gespiton <1044750905@qq.com>
2016-09-10 22:15:44 +0800

    fix multi load problem

--- elec_auto_search.js
+++ elec_auto_search.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         elec tracer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       sher
 // @match        http://elec.xmu.edu.cn/PdmlWebSetup/Pages/SMSMain.aspx
 // @grant        none
 // ==/UserScript==
-window.addEventListener ("load", get, false);
-function get(){
+
+//window.addEventListener ("load", get, false);
+if(document.getElementById("drlou").value!="2号楼"){
         document.getElementById("drxiaoqu").value = "09";
         javascript: setTimeout('__doPostBack(\'drxiaoqu\',\'\')', 0);
+        //alert("hello, master");
         document.getElementById("drlou").value = "2号楼";
         document.getElementById("txtRoomid").value = "0621";
         document.getElementById("dxbtnQuery_B").click();
